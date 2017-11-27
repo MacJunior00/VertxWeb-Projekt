67fd3774199b66b7e2329f9659b9cdefa941f34c
k.engler <k.engler@IR1-PC-12.musterschule.schule.paedml>
2017-11-27 17:18:38 +0100

    Session Handler hinzugefügt

--- src/main/java/de/qreator/vertx/VertxWebFormular.java
+++ src/main/java/de/qreator/vertx/VertxWebFormular.java
@@ -12,7 +12,12 @@ import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.Session;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
+import io.vertx.ext.web.sstore.SessionStore;
 
 public class VertxWebFormular {
 
@@ -22,23 +27,36 @@ public class VertxWebFormular {
         HttpServer server = vertx.createHttpServer();
 
         Router router = Router.router(vertx);
-
+        SessionStore store = LocalSessionStore.create(vertx);
+         SessionHandler sessionHandler = SessionHandler.create(store);
+         router.route().handler(CookieHandler.create());
+            router.route().handler(sessionHandler);
+        
+        
         router.route("/anfrage").handler(routingContext -> {
+            Session session = routingContext.session();
+            
+
+           
+            session.put("name", "passwort");
+            int age = session.get("age");
+           
             String typ = routingContext.request().getParam("typ");
             String name = routingContext.request().getParam("name");
-             String passwort = routingContext.request().getParam("password");
+            String passwort = routingContext.request().getParam("password");
             HttpServerResponse response = routingContext.response();
             response.putHeader("content-type", "application/json");
             JsonObject jo = new JsonObject();
 
-            if (typ.equals("namenKnopf")&&passwort.equals ("1234")) {
-               
+            if (typ.equals("namenKnopf") && passwort.equals("1234")) {
+
                 jo.put("typ", "antwort");
-                jo.put("text", "Der Text war " + name);
-             
-                jo.put("passwort","Das Passwort war " + passwort);
-            }
-            else {jo.put("passwort","Falsches Passwort");
+                jo.put("text", "Der Benutzer ist " + name);
+
+                jo.put("passwort", "Das Passwort war " + passwort);
+            } else {
+                jo.put("passwort", "Falsches Passwort");
+
             };
             response.end(Json.encodePrettily(jo));
         });
